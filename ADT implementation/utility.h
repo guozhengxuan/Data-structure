e122b102c4c6b525e1916c44ea1a812ecaf49d77
Archer <49683563+guozhengxuan@users.noreply.github.com>
2020-01-03 16:21:35 +0800

    Delete utility.h

--- ADT implementation/utility.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-enum Error_code
-{
-  success, fail, range_error, underflow, overflow, fatal, not_present, \
-  duplicate_error, entry_inserted, entry_found, internal_error
-};
